12ace0de8c3a29eda9a4ac1d5592a0acad1a69fd
Ricky Goldman <luigijerk@yahoo.com>
2015-06-19 17:20:37 -0500

    sef urls and dashboard

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,52 +1,32 @@
 <?php
+use yii\helpers\Url;
+
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+    <h1>Dashboard</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+    <h2>Contests</h2>
+    <p>
+        <a class="btn btn-lg btn-primary" href="<?=Url::to(['contest-current/index'])?>">Current</a>
+        <a class="btn btn-lg btn-primary" href="<?=Url::to(['contest-category/index'])?>">Categories</a>
+        <a class="btn btn-lg btn-primary" href="<?=Url::to(['contest-type/index'])?>">Types</a>
+        <a class="btn btn-lg btn-primary" href="<?=Url::to(['structure/index'])?>">Structures</a>
+    </p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
+    <h2>Locations</h2>
+    <p>
+        <a class="btn btn-lg btn-success" href="<?=Url::to(['location/index'])?>">Locations</a>
+        <a class="btn btn-lg btn-success" href="<?=Url::to(['subregion/index'])?>">Subregions</a>
+        <a class="btn btn-lg btn-success" href="<?=Url::to(['region/index'])?>">Regions</a>
+    </p>
 
-    <div class="body-content">
+    <h2>Users</h2>
+    <p>
+        <a class="btn btn-lg btn-danger" href="<?=Url::to(['user/index'])?>">Users</a>
+    </p>
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
 </div>
--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,20 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            // Disable index.php
+            'showScriptName' => false,
+            // Disable r= routes
+            'enablePrettyUrl' => true,
+            'rules' => array(
+                '' => 'site/index',
+                '<action:(about|contact|signup|login|logout)>' => 'site/<action>',
+                '<controller:\w+>/' => '<controller>/index',
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ),
+        ],
     ],
 ];
